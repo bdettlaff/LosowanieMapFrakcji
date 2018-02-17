df9db3b8f5ce4f0f3fc1c3c522b10ab506b551c9
Bartłomiej Dettlaff <bartlomiejdetttlaff@interia.pl>
2018-02-17 02:31:19 +0100

    Dodanie funkcjonalności Pokaż Mapy, Pokaż Frakcje oraz Instrukcja

---
+++ src/pokazFrakcje.java
@@ -0,0 +1,13 @@
+
+public class pokazFrakcje {
+
+	String[] naglowek = {"Numer","Nazwa frakcji"};
+
+	Object[][] tablica= 
+			{
+					{1,"Ksiêstwo Moskiewskie"},	
+					{2,"Rzeczpospolita"},
+					{3,"Kozacy Zaporoscy"},
+					{4,"Królestwo Szwecji"},
+			};
+}
---
+++ src/pokazMapy.java
@@ -0,0 +1,21 @@
+public class pokazMapy {
+	
+	String[] naglowek = {"Numer","Nazwa mapy"};
+
+	Object[][] tablica= 
+			{
+					{1,"Arabskie miasto"},	
+					{2,"Arena"},
+					{3,"Chutor"},
+					{4,"Leœna droga"},
+					{5,"Pagórkowata droga"},
+					{6,"Pola"},
+					{7,"Rynek"},
+					{8,"Skraj lasu"},
+					{9,"Tatarskie miasto"},
+					{10,"Ufortyfikowane miasto"},
+					{11,"Wioska nad rzek¹"},
+					{12,"Wioska w ruinach"},
+			};
+
+}
